f67089678a4ece0428f524b13403a60bce4f8793
sureshrestha <sureshrestha22@gmail.com>
2022-04-22 14:54:26 -0400

    Commiting the objects

---
+++ order-place/src/test/java/xom/apriltraining/orderplace/OrderPlacementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package xom.apriltraining.orderplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderPlacementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
